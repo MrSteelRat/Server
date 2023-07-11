b2249b158d69fcea04e1da09d7a2bc5964c4f13a
Ботев Александр Петрович <botev_ap@rawenstvo.ru>
2023-07-11 10:34:03 -0400

    add coonect to class server_base

--- server_base.cpp
+++ server_base.cpp
@@ -1,7 +1,53 @@
 #include "server_base.h"
 
-serverBase::serverBase()
-{
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QList>
 
-}
+class ChatServer : public QTcpServer {
 
+public:
+    explicit ChatServer(QObject* parent = nullptr) : QTcpServer(parent) {}
+
+protected:
+    void incomingConnection(qintptr socketDescriptor) override {
+        QTcpSocket* clientSocket = new QTcpSocket(this);
+        clientSocket->setSocketDescriptor(socketDescriptor);
+
+        connect(clientSocket, &QTcpSocket::readyRead, this, [=]() {
+            onReadyRead(clientSocket);
+        });
+
+        connect(clientSocket, &QTcpSocket::disconnected, this, [=]() {
+            onDisconnected(clientSocket);
+        });
+
+        clientSockets.append(clientSocket);
+        emit clientConnected(clientSocket->peerAddress().toString());
+
+        qDebug() << "Client connected:" << clientSocket->peerAddress().toString();
+    }
+
+private slots:
+    void onReadyRead(QTcpSocket* clientSocket) {
+        QByteArray data = clientSocket->readAll();
+        emit messageReceived(clientSocket->peerAddress().toString(), data);
+    }
+
+    void onDisconnected(QTcpSocket* clientSocket) {
+        clientSockets.removeOne(clientSocket);
+        emit clientDisconnected(clientSocket->peerAddress().toString());
+
+        qDebug() << "Client disconnected:" << clientSocket->peerAddress().toString();
+
+        clientSocket->deleteLater();
+    }
+
+signals:
+    void messageReceived(const QString& sender, const QByteArray& message);
+    void clientConnected(const QString& clientAddress);
+    void clientDisconnected(const QString& clientAddress);
+
+private:
+    QList<QTcpSocket*> clientSockets;
+};
--- server_base.h
+++ server_base.h
@@ -1,22 +1,11 @@
 #ifndef SERVERBASE_H
 #define SERVERBASE_H
 
-#include <iostream>
-#include <memory>
-#include <grpcpp/grpcpp.h>
-
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
 
 class serverBase
 {
 public:
     serverBase();
-
-private:
-    std::string serverAddrees;
 };
 
 #endif // SERVERBASE_H
